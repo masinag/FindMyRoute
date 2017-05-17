29dd0573e02b1e06fb0c5d6722a0cd6b5edae02d
masinag <gabriele.masina@virgilio.it>
2017-05-17 21:21:10 +0200

    Have managed to show gps track. Still to fix

--- views/itinerari.php
+++ views/itinerari.php
@@ -11,50 +11,11 @@
             <h1>Itinerari</h1>
         </header>
 
+        <?php include "prova_mappe.php" ?>
         <section class="w3-container">
             <div class="w3-row">
                 <div class="w3-third w3-container w3-green">
                     <h2>w3-third</h2>
-                    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCxNPdObgBGVgg7PJPj3KihhwnMr30kSzA&callback=initMap"
-                        type="text/javascript"></script>
-                    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-                    <script type="text/javascript">
-                      function initialize() {
-                        var map = new google.maps.Map(document.getElementById("map_canvas"), {
-                          mapTypeId: google.maps.MapTypeId.TERRAIN
-                        });
-
-                        $.ajax({
-                         type: "GET",
-                         url: "/FindMyRoute/files/tracks/2.gpx",
-                         dataType: "xml",
-                         success: function(xml) {
-                           var points = [];
-                           var bounds = new google.maps.LatLngBounds ();
-                           $(xml).find("trkpt").each(function() {
-                             var lat = $(this).attr("lat");
-                             var lon = $(this).attr("lon");
-                             var p = new google.maps.LatLng(lat, lon);
-                             points.push(p);
-                             bounds.extend(p);
-                           });
-
-                           var poly = new google.maps.Polyline({
-                             // use your own style here
-                             path: points,
-                             strokeColor: "#FF00AA",
-                             strokeOpacity: .7,
-                             strokeWeight: 4
-                           });
-
-                           poly.setMap(map);
-
-                           // fit bounds to track
-                           map.fitBounds(bounds);
-                         }
-                        });
-                      }
-</script>
                 </div>
                 <div class="w3-third w3-container">
                     <h2>w3-third</h2>
---
+++ views/prova_mappe.php
@@ -0,0 +1,85 @@
+<?php
+    $pluginDir = "../wp-gpx-maps/"; // wordpress plugin path
+    $gpxUrl = "../files/tracks/1.gpx"; // your gpx file
+
+    /* wordpress function wrapper */
+    function load_plugin_textdomain(){}
+    function is_admin() {return false;}
+    function add_action(){}
+    function add_shortcode(){}
+    function register_activation_hook(){}
+    function register_deactivation_hook(){}
+    function add_filter(){}
+    function get_option(){return "";}
+    function wp_upload_dir(){
+        $uploadDir = "/FindMyRoute/uploads";
+        return Array ( "basedir" => $uploadDir ); }
+    function plugins_url(){
+        $uploadUrl = "/FindMyRoute"; // gpx download path
+        return $uploadUrl ;}
+    function __($val){return $val;}
+
+    include $pluginDir.'wp-gpx-maps.php';
+?>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!-- <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script> -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCxNPdObgBGVgg7PJPj3KihhwnMr30kSzA" type="text/javascript"></script>
+    <!-- <script src="https://maps.googleapis.com/maps/api/js?sensor=false&v=3.9" type="text/javascript" ></script> -->
+    <script src="http://code.highcharts.com/highcharts.js" type="text/javascript" ></script>
+    <script src="<?php echo $pluginDir; ?>WP-GPX-Maps.js" type="text/javascript" ></script>
+    <?php
+    echo print_WP_GPX_Maps_styles();
+    ?>
+    <?php
+
+    /*
+    plugin call with all the possible settings
+    more info here: http://wordpress.org/extend/plugins/wp-gpx-maps/faq/
+    */
+
+    echo handle_WP_GPX_Maps_Shortcodes( Array (
+    "gpx"=> $gpxUrl,
+    "width"=> "100%",
+    "mheight"=> "450px",
+    "mtype"=>"HYBRID",
+    "gheight"=> "200px",
+    "showcad"=>false,
+    "showhr"=> false,
+    "waypoints"=> false,
+    "showspeed"=> false,
+    "showgrade"=> false,
+    "zoomonscrollwheel"=> false,
+    "donotreducegpx"=> false,
+    "pointsoffset"=> 10,
+    "uom"=>"0",
+    "uomspeed"=>"0",
+    "mlinecolor"=>"#ff0000",
+    "glinecolor"=>"#3366cc",
+    "glinecolorspeed"=>"#ff0000",
+    "glinecolorhr"=>"#ff77bd",
+    "glinecolorcad"=>"#beecff",
+    "glinecolorgrade"=>"#beecff",
+    "chartfrom1"=>"",
+    "chartto1"=>"",
+    "chartfrom2"=>"",
+    "chartto2"=> "",
+    "starticon"=> "",
+    "endicon"=> "",
+    "currenticon"=> "",
+    "waypointicon"=> "",
+    "nggalleries"=> "",
+    "ngimages"=>"",
+    "download"=> "",
+    "dtoffset"=> 0,
+    "skipcache"=> "",
+    "summary"=> "",
+    "summarytotlen"=> false,
+    "summarymaxele"=> false,
+    "summaryminele"=> false,
+    "summaryeleup"=>false,
+    "summaryeledown"=> false,
+    "summaryavgspeed"=> false,
+    "summarytotaltime"=>false,
+    ) );
+
+    ?>
