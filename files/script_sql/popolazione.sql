3166d226b284418f54b085c11a02de58a58d066f
iti5b04 <iti5b04@curie.local>
2017-05-19 09:35:36 +0200

    Update db population

--- files/script_sql/popolazione.sql
+++ files/script_sql/popolazione.sql
@@ -13,24 +13,39 @@ insert into province(id, sigla, nome) values
     (3, "VI", "Vicenza");
 
 insert into localita(id, CAP, nome, idProvincia) values
-    (1, 39040, "Salorno",            2),
-    (2, 38060, "Borghetto",          1),
-    (3, 38057, "San Cristoforo",     1),
-    (4, 36061, "Bassano del Grappa", 3),
-    (5, 38050, "Imer",               1),
-    (6, 38054, "Fiera di Primiero",  1),
-    (7, 38024, "Cogolo di Pejo",     1),
-    (8, 38020, "Mostizzolo",         1);
+    (1, 39040, "Salorno",           		 2),
+    (2, 38060, "Borghetto",         		 1),
+    (3, 38057, "San Cristoforo",    		 1),
+    (4, 36061, "Bassano del Grappa", 		 3),
+    (5, 38050, "Imer",              		 1),
+    (6, 38054, "Fiera di Primiero",  		 1),
+    (7, 38024, "Cogolo di Pejo",    		 1),
+    (8, 38020, "Mostizzolo",        		 1),
+	(9, 38032, "Canazei",           		 1),
+	(10,38030, "Castello Molina di fiemme",  1),
+	(11,38065, "Mori",                		 1),
+	(12,38066, "Riva del Garda",             1),
+	(13,38030, "Fontanazzo",               	 1),
+	(14,38070, "Ragoli",                     1),
+	(15,38086, "Pinzolo",               	 1);
 
 insert into puntiSignificativi(id, nome, tipo, sitoWeb, latitudine, longitudine, idLocalita) values
-    (1, "Confine con la Provincia di Bolzano", "altro",     null, 46.231029, 11.176574, 1),
-    (2, "Confine con la provincia di Verona",  "altro",     null, 45.699304, 10.925329, 2),
-    (3, "Stazione FS San Cristoforo - Ischia", "altro",     null, 46.039763, 11.234038, 3),
-    (4, "Centro storico Bassano del Grappa", "interesse",   null, 45.768103, 11.730397, 4),
-    (5, "Rotonda di Imer",                     "altro",     null, 46.147452, 11.795902, 5),
-    (6, "Centro storico di Fiera di Primiero", "interesse", null, 46.175914, 11.831815, 6),
-    (7, "Strada Provinciale 87",                "altro",    null, 46.349270, 10.693864, 7),
-    (8, "Ponte di Mostizzolo",                  "altro",    null, 46.392959, 11.013063, 8);
+    (1, "Confine con la Provincia di Bolzano", "altro",     null, 46.231029, 11.176574,  1),
+    (2, "Confine con la provincia di Verona",  "altro",     null, 45.699304, 10.925329,  2),
+    (3, "Stazione FS San Cristoforo - Ischia", "altro",     null, 46.039763, 11.234038,  3),
+    (4, "Centro storico Bassano del Grappa", "interesse",   null, 45.768103, 11.730397,  4),
+    (5, "Rotonda di Imer",                     "altro",     null, 46.147452, 11.795902,  5),
+    (6, "Centro storico di Fiera di Primiero", "interesse", null, 46.175914, 11.831815,  6),
+    (7, "Strada Provinciale 87",                "altro",    null, 46.349270, 10.693864,  7),
+    (8, "Ponte di Mostizzolo",                  "altro",    null, 46.392959, 11.013063,  8),
+	(9, "Via del Piz",                          "altro",    null, 46.474916, 11.770124,  9),
+    (10,"Località Arodolo",                     "altro",    null, 46.272876, 11.432851, 10),
+	(11,"Località alla Gorga",                  "altro",    null, 45.852523, 10.998959, 11),
+    (12,"Via Edoardo Modl",                     "altro",    null, 45.894535, 10.852044, 12),
+	(13,"Via Prada Quirinio",                   "altro",    null, 46.271370, 11.417393, 10),
+    (14,"Via Albolina",                         "altro",    null, 46.468440, 11.730564, 13),
+	(15,"Via Trento",                           "altro",    null, 46.056021, 10.811796, 14),
+    (16,"Via Ronch",                            "altro",    null, 46.177995, 10.772342, 15);
 
 insert into itinerari (id, nome, descrizione, lunghezza, tempoPercorrenza, difficolta,
     infoUtili, tracciaGPS, idPuntoPartenza, idPuntoArrivo) values
@@ -58,7 +73,23 @@ insert into itinerari (id, nome, descrizione, lunghezza, tempoPercorrenza, diffi
     (4, "Pista Ciclabile Val di Sole",
     "La pista ciclabile della Val di Sole si sviluppa quasi per intero lungo il percorso dello spumeggiante Fiume Noce, per quasi 35 km, da Cogolo di Pejo a Fucine e quindi a Mostizzolo ricalcando il tracciato di antiche strade di collegamento o di strade arginali e di campagna. Il percorso non è impegnativo e copre un dislivello complessivo di 565 metri in discesa.
     Si possono scegliere vari percorsi ed utilizzare il treno Dolomiti Express e il Bici Bus, appositamente attrezzati per il trasporto delle bici, per integrare gli itinerari o risalire comodamente dal fondovalle. Le biciclette sono noleggiabili presso i numerosi noleggi presenti in tutta la valle.",
-    33.80, "03:00:00", "2", null, "4.gpx", 7, 8);
+    33.80, "03:00:00", "2", null, "4.gpx", 7, 8),
+
+	(5, "Ciclabile Delle Dolomiti di Fiemme e Fassa",
+    "La pista ciclabile delle Dolomiti di Fiemme e Fassa collega le 2 valli fra prati, ruscelli e masi di montagna. Con un dislivello di appena 600 metri, si possono percorrere 48 km, da Molina di Fiemme fino a Canazei oppure pedalare in discesa, da Canazei a Molina di Fiemme.",
+    44.60, "03:30:00", "1", null, "5.gpx", 9, 10),
+
+	(6, "Pista Ciclabile Del Basso Sarca",
+    "La pista congiunge la Vallagarina alla zona del lago di Garda, ed è collegata alla pista ciclabile della valle dell'Adige che passa da Trento e Rovereto.",
+    18.20, "01:15:00", "1", null, "6.gpx", 11, 12),
+
+	(7, "Pista Ciclabile Val di Fassa",
+    "La pista si sviluppa per 7 km, collega Moena a Pozza di Fassa e continua il tracciato ciclopedonale iniziato a Molina di Fiemme.",
+    42.00, "02:00:00", "1", null, "7.gpx", 13, 14),
+
+	(8, "Pista Ciclabile Val Rendena",
+    "La ciclopista segue il corso del fiume Sarca e misura complessivamente circa 20 km, partendo da Tione di Trento (presso l’ampio parcheggio presso il centro sportivo di Sesena) per concludersi a Carisolo",
+    26.20, "03:15:00", "1", null, "8.gpx", 15, 16);
 
 insert into immagini (id, path, idItinerario) values
     (1, "1_1.jpg", 1),
@@ -70,4 +101,12 @@ insert into immagini (id, path, idItinerario) values
     (7, "3_1.jpg", 3),
     (8, "3_2.jpg", 3),
     (9, "4_1.jpg", 4),
-    (10,"4_2.jpg", 4);
+    (10,"4_2.jpg", 4),
+	(11,"5_1.jpg", 5),
+    (12,"5_2.jpg", 5),
+	(13,"6_1.jpg", 6),
+    (14,"6_2.jpg", 6),
+	(15,"7_1.jpg", 7),
+    (16,"7_2.jpg", 7),
+	(17,"8_1.jpg", 8),
+    (18,"8_2.jpg", 8);
